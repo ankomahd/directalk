f292ccfc297f7390e38e5420a614bec0ab7a4442
Kofi Hagan <khagan@4sightmicrosystems.com>
2013-07-15 21:33:32 +0000

    added has_many messages to project

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
   belongs_to :project
-  attr_accessible :content, :start_date, :title
+  attr_accessible :content, :start_date, :title, :project_id
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
+	has_many :messages
   attr_accessible :title
 end
